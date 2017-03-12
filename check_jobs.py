37bf805e49b955c0dbe5af581f74f5b0651f723a
Jamie Sherriff <sherriffjamie@gmail.com>
2017-03-12 17:16:25 +1300

    add the ability to iterate over a list of css items, also some tidy up

--- check_jobs.py
+++ check_jobs.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python
+__author__ = "Jamie Sherriff"
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jamie Sherriff"
+__contact__ = "https://github.com/jamie-sherriff"
+__status__ = "Production"
+__date__ = "12-03-2017"
 '''
-install requirements:
-pip install requests
-pip install pyquery
+Simple script to check various webpages if jobs are available, reads in the url to scan from sys.argv
+see requirements.txt for pip dependencies
 '''
-
 import requests
 from pyquery import PyQuery
 import traceback
@@ -14,7 +20,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from urllib.parse import urlparse
 from email_client import send_email
-from constants import JOB_REGEX
+from constants import JOB_REGEX, JOB_CSS_SEARCH_LIST
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(os.path.splitext(os.path.basename(sys.argv[0]))[0])
@@ -48,17 +54,28 @@ try:
     response.raise_for_status()
     raw_html = response.text
     jQuery = PyQuery(raw_html)
-    job_list = jQuery('.job-list').text()
-    match = JOB_REGEX.search(job_list)
-    email_message['Subject'] = "Jobs Available at: " + SERVER_HOST
-    body = "Content Retrieved from " + SERVER_HOST + ":\n" + job_list
-    email_message.attach(MIMEText(body, 'plain'))
-    logger.debug('Found job_list: ' + job_list)
-    if not match:
-        logger.info('Jobs have appeared so sending email.')
+    job_list = None
+    for job_css in JOB_CSS_SEARCH_LIST:
+        job_list = jQuery(job_css).text()
+        if job_list:
+            break
+    if not job_list:
+        logger.info('Running job checker against: ' + SERVER_HOST + 'and could not parse page')
+        email_message['Subject'] = "Job Checker could not parse page at: " + SERVER_HOST
+        body = "Raw HTML FROM: " + SERVER_HOST + ":\n" + jQuery.html(method='html')
+        email_message.attach(MIMEText(body, 'plain'))
         send_email(email, email_message)
     else:
-        logger.info('Found no jobs so not sending email')
+        match = JOB_REGEX.search(job_list)
+        email_message['Subject'] = "Jobs Available at: " + SERVER_HOST
+        body = "Content Retrieved from " + SERVER_HOST + ":\n" + job_list
+        email_message.attach(MIMEText(body, 'plain'))
+        logger.debug('Found job_list: ' + job_list)
+        if not match:
+            logger.info('Jobs have appeared so sending email.')
+            send_email(email, email_message)
+        else:
+            logger.info('Found no jobs so not sending email')
 except Exception as error:
     logger.error('Found error in email process:', exc_info=True)
     email_message['Subject'] = SERVER_HOST + " Job Emailer Fault"
--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
 import re
 JOB_REGEX = re.compile('no vacancies', re.IGNORECASE | re.UNICODE | re.MULTILINE)
+JOB_CSS_SEARCH_LIST = ['.job-list']
